cfe770375de5d58e662de06b07d0e1ff3250d4dc
Manyu Lakhotia <lakemonyew@gmail.com>
2024-07-21 11:31:28 -0500

    some query work

--- Program.fs
+++ Program.fs
@@ -1,6 +1,75 @@
 open Acadian.FSharp
+open Acadian.Dapper.Fs
+open FSharp.Control.Tasks.V2.ContextInsensitive
+open Dapper
+open Npgsql
 
-type Action = unit -> Async<unit>
+type Task = {
+  // id: string array
+  // QueueNames: string array
+  Type: string
+  Status: string
+  // Payload: string
+  // Result: string
+  ProgramPath: string option
+  ProgramType: string option
+  // CreatedAt: string
+  // ExecutedAt: string
+  // TimeElapsed: string
+}
+
+let connectionString = "Host=127.0.0.1:5432;Database=postgres;Username=postgres;Password=postgres"
+
+type Data() =
+  // member this.getConn() = safeSqlConnection connectionString
+  member this.getConn() = new NpgsqlConnection(connectionString)
+
+
+  member this.getTasks() = task {
+    let x = Unchecked.defaultof<Task>
+    let sql = $"
+      select
+        --  = Id
+        -- queue_names
+        {nameof x.Type} = type
+        ,status
+        -- ,payload
+        -- ,result
+        ,{nameof x.ProgramPath} = program_path
+        ,{nameof x.ProgramType} = program_type
+        -- , = created_at
+        -- , = executed_at
+        -- , = time_elapsed
+      from Task
+      ;
+    "
+    use conn = this.getConn()
+    let! tasks = conn.QueryAsync<Task>(sql) |> Task.map (Option.ofObjForce)
+    return tasks |> Option.map Seq.toList
+  }
+
+  // member this.getTasks() = task {
+  //   let x = Unchecked.defaultof<Task>
+  //   let sql = $"
+  //     select
+  //       --  = Id
+  //       -- queue_names
+  //       {nameof x.Type} = type
+  //       ,status
+  //       -- ,payload
+  //       -- ,result
+  //       ,{nameof x.ProgramPath} = program_path
+  //       ,{nameof x.ProgramType} = program_type
+  //       -- , = created_at
+  //       -- , = executed_at
+  //       -- , = time_elapsed
+  //     from Task
+  //     ;
+  //   "
+  //   use conn = this.getConn()
+  //   let! items = conn.QueryAsync<Task>(sql) |> Task.map (Option.ofObjForce)
+  //   return items |> Option.map Seq.toList
+  // }
 
 let timeTillNextPollMs = 3000
 
@@ -19,7 +88,10 @@ let rec processQueue () = async {
 }
 
 let mainAsync _argv = async {
-  do! processQueue ()
+  let data = Data()
+  let! tasks = data.getTasks() |> Async.AwaitTask
+  printfn "%A" tasks
+  // do! processQueue ()
   return 0
 }
 
