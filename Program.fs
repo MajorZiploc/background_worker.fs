bafdc7e24b2007d00805016b5dca5ad451ffc92c
Manyu Lakhotia <lakemonyew@gmail.com>
2024-07-21 11:45:05 -0500

    more query attempts

--- Program.fs
+++ Program.fs
@@ -3,6 +3,7 @@ open Acadian.Dapper.Fs
 open FSharp.Control.Tasks.V2.ContextInsensitive
 open Dapper
 open Npgsql
+open Npgsql.FSharp
 
 type Task = {
   // id: string array
@@ -24,6 +25,13 @@ type Data() =
   // member this.getConn() = safeSqlConnection connectionString
   member this.getConn() = new NpgsqlConnection(connectionString)
 
+  member this.getConnStr() =
+    Sql.host "localhost"
+    |> Sql.database "postgres"
+    |> Sql.username "postgres"
+    |> Sql.password "postgres"
+    |> Sql.port 5432
+    |> Sql.formatConnectionString
 
   member this.getTasks() = task {
     let x = Unchecked.defaultof<Task>
@@ -40,13 +48,44 @@ type Data() =
         -- , = created_at
         -- , = executed_at
         -- , = time_elapsed
-      from Task
+      from \"Task\"
       ;
     "
     use conn = this.getConn()
     let! tasks = conn.QueryAsync<Task>(sql) |> Task.map (Option.ofObjForce)
     return tasks |> Option.map Seq.toList
   }
+
+  member this.getTasks1() =
+    let x = Unchecked.defaultof<Task>
+    let sql = $"
+      select
+        --  = Id
+        -- queue_names
+        {nameof x.Type} = type
+        ,{nameof x.Status} = status
+        -- ,payload
+        -- ,result
+        ,{nameof x.ProgramPath} = program_path
+        ,{nameof x.ProgramType} = program_type
+        -- , = created_at
+        -- , = executed_at
+        -- , = time_elapsed
+      from \"Task\"
+      ;
+    "
+    this.getConnStr ()
+    |> Sql.connect
+    |> Sql.query sql
+    |> Sql.execute (fun read ->
+        {
+            Type = read.text "Type"
+            Status = read.text "Status"
+            ProgramPath = read.textOrNone "ProgramPath"
+            ProgramType = read.textOrNone "ProgramType"
+            // FirstName = read.int "first_name"
+            // LastName = read.textOrNone "last_name" // reading nullable column
+        })
 
   // member this.getTasks() = task {
   //   let x = Unchecked.defaultof<Task>
@@ -89,7 +128,8 @@ let rec processQueue () = async {
 
 let mainAsync _argv = async {
   let data = Data()
-  let! tasks = data.getTasks() |> Async.AwaitTask
+  // let! tasks = data.getTasks1() |> Async.AwaitTask
+  let tasks = data.getTasks1()
   printfn "%A" tasks
   // do! processQueue ()
   return 0
