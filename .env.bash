7cc9cfcc76cda75dc487937dce988224dce0c398
Manyu Lakhotia <lakemonyew@gmail.com>
2024-09-14 08:19:04 -0500

    +.env.bash for local runs

---
+++ .env.bash
@@ -0,0 +1,17 @@
+export POSTGRES_USER='postgres'
+export POSTGRES_PASSWORD='password'
+export POSTGRES_PORT=5432
+export POSTGRES_DB="postgres"
+# naming convertion of the above vars but for the db container
+export PGHOST='pgsql'
+export PGUSER='postgres'
+export PGPASSWORD='password'
+export PGPORT=5432
+export PGDATABASE="postgres"
+export PGDATA="/var/lib/postgresql/data/"
+export PGDATALOCAL="/tmp/background_worker-fs_pgsql"
+
+export QUEUES="main,internal,external"
+export MACHINE_NAME="task_master"
+export TIME_TILL_NEXT_POLL_MS=3000
+export TASK_COUNT=20
